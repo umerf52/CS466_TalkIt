bc5ce7f32c8726e88e9a09353ec02583e1bef0fd
Saad Khan <saadahmadkhan98@gmail.com>
2019-11-19 10:39:57 +0500

    Bottom bar added

--- app/src/main/java/com/apps/talkit/FeedActivity.java
+++ app/src/main/java/com/apps/talkit/FeedActivity.java
@@ -1,5 +1,6 @@
 package com.apps.talkit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +17,9 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
 
 import java.util.ArrayList;
 
@@ -40,6 +44,29 @@ public class FeedActivity extends BaseActivity {
         recyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new RecyclerViewAdapterPosts(this, postsList);
         recyclerView.setAdapter(mAdapter);
+
+        SpaceNavigationView spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
+        spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
+        spaceNavigationView.addSpaceItem(new SpaceItem("Feed",R.drawable.feed));
+        spaceNavigationView.addSpaceItem(new SpaceItem("Therapy", R.drawable.therapy));
+        spaceNavigationView.changeCurrentItem(0);
+
+        spaceNavigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+                Toast.makeText(getTalkitContext(),"onCentreButtonClick", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+                Toast.makeText(getTalkitContext(), itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+                Toast.makeText(getTalkitContext(), itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+        });
 
         FirebaseApp.initializeApp((this));
         db = FirebaseFirestore.getInstance();
@@ -72,4 +99,5 @@ public class FeedActivity extends BaseActivity {
             }
         });
     }
+
 }
--- app/src/main/java/com/apps/talkit/HomepageActivity.java
+++ app/src/main/java/com/apps/talkit/HomepageActivity.java
@@ -6,10 +6,14 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.apps.talkit.classes.UserInfo;
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
 
 import java.util.ArrayList;
 
@@ -21,6 +25,7 @@ public class HomepageActivity extends BaseActivity {
     private ArrayList<String> chats = new ArrayList<>();
     private RecyclerView.Adapter mAdapterN;
     private RecyclerView.Adapter mAdapterC;
+    private SpaceNavigationView spaceNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,33 @@ public class HomepageActivity extends BaseActivity {
         recyclerViewC.setLayoutManager(mLayoutManagerC);
         mAdapterC = new RecyclerViewAdapterChats(this, chats);
         recyclerViewC.setAdapter(mAdapterC);
+
+        spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
+        spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
+        spaceNavigationView.addSpaceItem(new SpaceItem("Feed",R.drawable.feed));
+        spaceNavigationView.addSpaceItem(new SpaceItem("Therapy", R.drawable.therapy));
+        spaceNavigationView.changeCurrentItem(-1);
+
+        spaceNavigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+                Toast.makeText(getTalkitContext(),"onCentreButtonClick", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+                Toast.makeText(getTalkitContext(), itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+                if(itemIndex==0){
+                    Intent myIntent = new Intent(HomepageActivity.this, FeedActivity.class);
+                    HomepageActivity.this.startActivity(myIntent);
+                }
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+                Toast.makeText(getTalkitContext(), itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void fillTheLists() {
@@ -74,6 +106,7 @@ public class HomepageActivity extends BaseActivity {
 
     @Override
     protected void onResume() {
+        spaceNavigationView.changeCurrentItem(-1);
         super.onResume();
         ((RecyclerViewAdapterNotifications) mAdapterN).setOnItemClickListener(new RecyclerViewAdapterNotifications.MyClickListener() {
             @Override
