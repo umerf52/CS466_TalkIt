4b724d1127a52e146f3c788abe2ba9d082fd9b51
muzammilLUMS <muzammilhussain1031@gmail.com>
2019-11-23 11:25:06 +0500

    Create Post Page Added.

--- app/src/main/java/com/apps/talkit/PostActivity.java
+++ app/src/main/java/com/apps/talkit/PostActivity.java
@@ -1,14 +1,18 @@
 package com.apps.talkit;
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ToggleButton;
 
 public class PostActivity extends BaseActivity {
 
+    ToggleButton toggleButton1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
-        EditText f= findViewById(R.id.Shareyourthoughts);
+        EditText f= findViewById(R.id.postContent2);
+
+        toggleButton1 = (ToggleButton) findViewById(R.id.t1);
 
     }
 }
