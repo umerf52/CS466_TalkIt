b15d2ebf0196f3ab7cdc6987b9e3876b318477f8
taimoor0217 <taimoor0217@gmail.com>
2019-11-23 17:14:27 +0500

    Allow Tags to be entered at runtime

--- app/src/main/java/com/apps/talkit/PostActivity.java
+++ app/src/main/java/com/apps/talkit/PostActivity.java
@@ -3,6 +3,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.ToggleButton;
 
 public class PostActivity extends BaseActivity {
@@ -13,13 +15,27 @@ public class PostActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
         EditText f= findViewById(R.id.postContent2);
-
-        toggleButton1 = (ToggleButton) findViewById(R.id.t1);
+//        LinearLayout lLayout = findViewById(R.id.tagscontainer); // Root ViewGroup in which you want to add textviews
+//        for (int i = 0; i < 5; i++) {
+//            TextView tv = new TextView(this); // Prepare textview object programmatically
+//            tv.setText("Dynamic TextView" + i);
+//            tv.setId(i + 5);
+//            lLayout.addView(tv); // Add to your ViewGroup using this method
+//        }
 
     }
     public void onShareButton(View v){
         Intent myIntent = new Intent(this, HomeActivity.class);
         startActivity(myIntent);
+    }
+    public void onAddTag(View v){
+        LinearLayout lLayout = findViewById(R.id.tagscontainer);
+        EditText t_name = findViewById(R.id.tagstext);
+        TextView tv = new TextView(this);
+        tv.setText("  "+ t_name.getText());
+//        tv.setId("1");
+        t_name.setText("");
+        lLayout.addView(tv);
     }
 
 
